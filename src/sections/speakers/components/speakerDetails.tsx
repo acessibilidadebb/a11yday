cc55f3432c0af45a07160b65c7086e6302f40b98
F9329129 Thiago Martins Neves <neves@bb.com.br>
2024-07-18 17:55:19 -0300

    Improved speakerDetails modal

--- src/sections/speakers/components/speakerDetails.tsx
+++ src/sections/speakers/components/speakerDetails.tsx
@@ -1,29 +1,55 @@
 import closeBtn from '../../../assets/close.png'
 import './speakerDetails.scss'
-import { SpeakerDetailsProps } from "../types"
+import { SpeakerDetailsProps } from '../types'
+import { useEffect, useState } from 'react'
 
 export function SpeakerDetails(props: SpeakerDetailsProps) {
   const { speaker, showDetails, setShowDetails } = props
+  const [scrollPosition, setScrollPosition] = useState(0)
+
+  useEffect(() => {
+    if (showDetails) {
+      // Armazenar a posição de rolagem atual e rolar para o topo
+      setScrollPosition(window.scrollY)
+      window.scrollTo(0, 0)
+      document.body.style.overflow = 'hidden' // Previne rolagem da página principal
+    } else {
+      // Restaurar a posição de rolagem ao fechar
+      document.body.style.overflow = '' // Restaura rolagem da página principal
+      window.scrollTo(0, scrollPosition)
+    }
+  }, [showDetails])
+
   const handleClose = () => {
     setShowDetails(false)
   }
+  const handleOverlayClick = (event) => {
+    if (event.target === event.currentTarget) {
+      handleClose()
+    }
+  }
   return (
-    <div className={`speaker-details-modal ${showDetails ? 'open' : ''}`}>
-      <button onClick={handleClose} className="close-btn" aria-label="Fechar">
-        <img src={closeBtn} alt="Icone de Fechar" />
-      </button>
+    <div
+      onClick={handleOverlayClick}
+      className={`speaker-details-modal ${showDetails ? 'open' : ''}`}
+    >
       <div className="speaker-details-container">
-        <section className="speaker-details-body">
-          <p className="speaker-details-about">Sobre a pessoa Palestrante</p>
-          <img
-            className="speaker-details-image"
-            src={`/palestrantes/${speaker.image}`}
-            alt={`Foto de ${speaker.seuNome}`}
-          />
-          <h4 className="speaker-details-name">{speaker.seuNome}</h4>
-          <p className="speaker-details-company">{speaker.empresa}</p>
-          <p className="speaker-details-description">{speaker?.miniBio}</p>
-        </section>
+        <div className="speaker-details-content">
+        <button onClick={handleClose} className="speaker-details-close-btn" aria-label="Fechar">
+          <img src={closeBtn} alt="Ícone de Fechar" />
+        </button>
+          <section className="speaker-details-body">
+            <p className="speaker-details-about">Sobre a pessoa Palestrante</p>
+            <img
+              className="speaker-details-image"
+              src={`/palestrantes/${speaker.image}`}
+              alt={`Foto de ${speaker.seuNome}`}
+            />
+            <h4 className="speaker-details-name">{speaker.seuNome}</h4>
+            <p className="speaker-details-company">{speaker.empresa}</p>
+            <p className="speaker-details-description">{speaker?.miniBio}</p>
+          </section>
+        </div>
       </div>
     </div>
   )
