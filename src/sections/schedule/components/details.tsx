c9eb04586268a368b1371d5bb843db1d0294f3e4
F9329129 Thiago Martins Neves <neves@bb.com.br>
2024-07-18 20:22:41 -0300

    Fixed schedule details modal

--- src/sections/schedule/components/details.tsx
+++ src/sections/schedule/components/details.tsx
@@ -1,7 +1,7 @@
 import closeBtn from '../../../assets/close.png'
 import speakersData from '../../../speakers.json'
 import { Speaker } from '../../../types/speakers'
-import { DetailsProps } from "../types"
+import { DetailsProps } from '../types'
 import './details.scss'
 
 export function Details(props: DetailsProps) {
@@ -18,27 +18,45 @@ export function Details(props: DetailsProps) {
     ),
     image: speaker.image,
   }))[0] as Speaker
+  const handleOverlayClick = (event: React.MouseEvent) => {
+    if (event.target === event.currentTarget) {
+      handleClose()
+    }
+  }
   return (
-    <div className={`details-modal ${showDetails ? 'open' : ''}`}>
-      <button onClick={handleClose} className="close-btn" aria-label="Fechar">
-        <img src={closeBtn} alt="Icone de Fechar" />
-      </button>
-      <article className="details-container">
-        <header className="details-header">
-          <h3 className="details-title">{title}</h3>
-          <p className="details-description">{speakerDetails.resumo}</p>
-        </header>
-        <section className="details-body">
-          <p className="details-about">Sobre a Palestrante</p>
-          <img
-            className="details-image"
-            src={`/palestrantes/${speakerDetails.image}`}
-            alt={`Foto de ${speakerDetails.seuNome}`}
-          />
-          <h4 className="details-name">{speakerDetails.seuNome}</h4>
-          <p className="details-company">{speakerDetails.empresa}</p>
-          <p className="details-description">{speakerDetails?.miniBio}</p>
-        </section>
+    <div
+      onClick={handleOverlayClick}
+      className={`schedule-details-modal ${showDetails ? 'open' : ''}`}
+    >
+      <article className="schedule-details-container">
+        <div className="schedule-details-content">
+          <button
+            onClick={handleClose}
+            className="schedule-details-close-btn"
+            aria-label="Fechar"
+          >
+            <img src={closeBtn} alt="Icone de Fechar" />
+          </button>
+          <header className="schedule-details-header">
+            <h3 className="schedule-details-title">{title}</h3>
+            <p className="schedule-details-description">
+              {speakerDetails.resumo}
+            </p>
+          </header>
+          <section className="schedule-details-body">
+            <p className="schedule-details-about">Sobre a Palestrante</p>
+            <img
+              className="schedule-details-image"
+              src={`/palestrantes/${speakerDetails.image}`}
+              alt={`Foto de ${speakerDetails.seuNome}`}
+            />
+            <h4 className="schedule-details-name">{speakerDetails.seuNome}</h4>
+            <p className="schedule-details-company">{speakerDetails.empresa}</p>
+            <p className="schedule-details-description">
+              {speakerDetails?.miniBio}
+            </p>
+          </section>
+        </div>
       </article>
     </div>
   )
