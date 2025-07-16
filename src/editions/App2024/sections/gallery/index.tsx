eb1701ed7a20413710b1956a96a256a9ccdbb265
Thiago Neves <thiagomn03@gmail.com>
2025-07-16 12:09:14 -0300

    Restaurada galeria

--- src/editions/App2024/sections/gallery/index.tsx
+++ src/editions/App2024/sections/gallery/index.tsx
@@ -1,16 +1,20 @@
+import ReactImageGallery from 'react-image-gallery'
 import 'react-image-gallery/styles/css/image-gallery.css'
+import galleryData from '../../json/galleryData.json'
 import './gallery.scss'
+
+const ImageGallery = ReactImageGallery as any
 
 export default function Gallery() {
   return (
     <div className="gallery">
       <div className="gallery-container">
-        {/* <ImageGallery
+        <ImageGallery
           items={galleryData}
           showThumbnails={true}
           autoPlay={false}
           slideInterval={3000}
-        /> */}
+        />
       </div>
     </div>
   )
