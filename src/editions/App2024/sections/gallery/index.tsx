bed70c361e7dbd485fb13afb09014e0965bf4d30
Thiago Neves <thiagomn03@gmail.com>
2025-07-16 12:16:26 -0300

    Melhorada tipagem da galeria

--- src/editions/App2024/sections/gallery/index.tsx
+++ src/editions/App2024/sections/gallery/index.tsx
@@ -2,8 +2,31 @@ import ReactImageGallery from 'react-image-gallery'
 import 'react-image-gallery/styles/css/image-gallery.css'
 import galleryData from '../../json/galleryData.json'
 import './gallery.scss'
+interface GalleryItem {
+  original: string
+  thumbnail: string
+  description?: string
+  originalAlt?: string
+  thumbnailAlt?: string
+}
+interface ImageGalleryProps {
+  items: GalleryItem[]
+  showThumbnails?: boolean
+  autoPlay?: boolean
+  slideInterval?: number
+  showPlayButton?: boolean
+  showFullscreenButton?: boolean
+  showNav?: boolean
+  showBullets?: boolean
+  infinite?: boolean
+  lazyLoad?: boolean
+  onSlide?: (currentIndex: number) => void
+  onScreenChange?: (fullScreenElement: Element | null) => void
+  onPlay?: (currentIndex: number) => void
+  onPause?: (currentIndex: number) => void
+}
 
-const ImageGallery = ReactImageGallery as any
+const ImageGallery = ReactImageGallery as unknown as React.ComponentType<ImageGalleryProps>
 
 export default function Gallery() {
   return (
